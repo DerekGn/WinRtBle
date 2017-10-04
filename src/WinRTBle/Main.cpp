28a6400e6e9885f15e283bf43d3c0eac415f4976
DerekGn <9FB2EFB6AF41A7C3>
2017-10-04 19:53:29 +0100

    initial commit

---
+++ src/WinRTBle/Main.cpp
@@ -0,0 +1,94 @@
+#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Devices::Bluetooth::Advertisement;
+
+std::wstring guidToString(GUID g)
+{
+	std::wostringstream ret;
+
+	ret << std::hex << std::setfill(L'0')
+		<< std::setw(2) << g.Data1 << ":"
+		<< std::setw(2) << g.Data2 << ":"
+		<< std::setw(2) << g.Data3 << ":"
+		<< std::setw(2) << g.Data4;
+
+	return ret.str();
+}
+
+std::wstring advertisementTypeToString(BluetoothLEAdvertisementType advertisementType)
+{
+	std::wstring ret;
+
+	switch (advertisementType)
+	{
+	case BluetoothLEAdvertisementType::ConnectableUndirected:
+		ret = L"ConnectableUndirected";
+		break;
+	case BluetoothLEAdvertisementType::ConnectableDirected:
+		ret = L"ConnectableDirected";
+		break;
+	case BluetoothLEAdvertisementType::ScannableUndirected:
+		ret = L"ScannableUndirected";
+		break;
+	case BluetoothLEAdvertisementType::NonConnectableUndirected:
+		ret = L"NonConnectableUndirected";
+		break;
+	case BluetoothLEAdvertisementType::ScanResponse:
+		ret = L"ScanResponse";
+		break;
+	default:
+		break;
+	}
+
+	return ret;
+}
+
+int main()
+{
+	init_apartment();
+
+	try
+	{
+		BluetoothLEAdvertisementWatcher watcher;
+
+		std::wcout << std::hex <<
+			"MaxOutOfRangeTimeout: [0x" << watcher.MaxOutOfRangeTimeout().count() << "]" << std::endl <<
+			"MaxSamplingInterval:  [0x" << watcher.MaxSamplingInterval().count() << "]" << std::endl <<
+			"MinOutOfRangeTimeout: [0x" << watcher.MinOutOfRangeTimeout().count() << "]" << std::endl <<
+			"MinSamplingInterval:  [0x" << watcher.MinSamplingInterval().count() << "]" << std::endl <<
+			std::endl;
+
+		watcher.ScanningMode(BluetoothLEScanningMode::Active);
+
+		watcher.Received([&](BluetoothLEAdvertisementWatcher watcher, BluetoothLEAdvertisementReceivedEventArgs eventArgs) {
+			
+			watcher.Stop();
+						
+			std::wcout <<
+				"LocalName: [" << eventArgs.Advertisement().LocalName().c_str() << "]" <<
+				"AdvertisementType: [" << advertisementTypeToString(eventArgs.AdvertisementType()) << "]" <<
+				"BluetoothAddress: [0x" << std::hex << eventArgs.BluetoothAddress() << "]" <<
+				"RawSignalStrengthInDBm: [" << std::dec << eventArgs.RawSignalStrengthInDBm() << "]" <<
+				std::endl;
+
+			auto serviceUuids = eventArgs.Advertisement().ServiceUuids();
+
+			for (GUID const & g : serviceUuids)
+				std::wcout << "ServiceUUID: [" << guidToString(g) << "]" << std::endl;
+
+		});
+
+		auto status = watcher.Status();
+
+		watcher.Start();
+
+		int a;
+		std::cin >> a;
+		return 0;
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+}
---
+++ src/WinRTBle/pch.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#pragma comment(lib, "windowsapp") 
+
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+#include "winrt/Windows.Devices.Bluetooth.h"
+#include "winrt/Windows.Devices.Bluetooth.Advertisement.h"
